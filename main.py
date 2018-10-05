9b524ec06a401c317e69d709ebe5937a34bb688d
zpr <Dennis.Zeipert@streetscooter.eu>
2018-10-05 14:58:01 +0200

    added some comments

--- main.py
+++ main.py
@@ -130,10 +130,10 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
     correct_label = tf.reshape(correct_label, (-1,num_classes))
     
-    # define loss function
+    # implement a suitable loss function
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits= logits, labels= correct_label))
     
-    # define training operation
+    # implement a suitable optimizer function
     optimizer = tf.train.AdamOptimizer(learning_rate= learning_rate)
     
     train_op = optimizer.minimize(cross_entropy_loss)
